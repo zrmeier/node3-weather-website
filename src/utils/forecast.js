09f376196aa2f222034f8975b2ff27ca2282b08e
Zach Meier <zrmeier@indiana.edu>
2019-08-16 17:11:42 -0400

    Edit Forecast String

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 const forecast = (longitude, latitude, callback) => {
     const url = 'https://api.darksky.net/forecast/9ec2ec4db2a263478c422ee1c33d6174/' + encodeURIComponent(longitude) + ',' + encodeURIComponent(latitude)
 
-    request({ url, json: true }, (error, { body}) =>{
+    request({ url, json: true }, (error, { body }) =>{
         if(error) {
             callback('Unable to connect to forecasting service!', undfined)
         }else if (body.error) {
             callback(body.error, undefined)
         }else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out.  There is a ' + body.currently.precipProbability + '% chance of rain for today.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out and humidity at ' + body.currently.humidity + '%.' + 'There is a ' + body.currently.precipProbability + '% chance of rain for today.')
         }
 
     })
